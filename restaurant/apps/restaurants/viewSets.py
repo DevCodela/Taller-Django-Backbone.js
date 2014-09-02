a1664cc8a553e7851cd691314fd175e617708b2a
JeanCarlos <jean_carlos104@hotmail.com>
2014-09-02 13:34:34 -0500

    julio

--- restaurant/apps/restaurants/viewSets.py
+++ restaurant/apps/restaurants/viewSets.py
@@ -1,4 +1,7 @@
+from django.http import Http404
+
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Restaurant
 from .serializers import RestaurantSerializer
@@ -6,5 +9,13 @@ from .serializers import RestaurantSerializer
 class RestaurantViewSet(viewsets.ModelViewSet):
 
 	model = Restaurant
-	serializer_class = RestaurantSerializer
-	paginate_by = 5
+	# serializer_class = RestaurantSerializer
+
+	def list(self, request):
+		try:
+			page = int(request.GET['page'])*5
+			restaurants = Restaurant.objects.all()[page-5:page]
+		except:
+			raise Http404
+		serializer = RestaurantSerializer(restaurants, many=True)
+		return Response(serializer.data)
