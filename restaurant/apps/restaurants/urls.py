cffbe53f9bfc0fcbc6a9c1b43da3307c9bde30c9
JeanCarlos <jean_carlos104@hotmail.com>
2014-09-02 17:11:27 -0500

    a

--- restaurant/apps/restaurants/urls.py
+++ restaurant/apps/restaurants/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import patterns, include, url
-
-from rest_framework import routers
-
-from .viewSets import RestaurantViewSet
 from .views import IndexView
-
-router = routers.DefaultRouter()
-router.register(r'restaurants', RestaurantViewSet)
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view()),
-    url(r'^api/', include(router.urls)),
 )
